268bfefb80cc2b3d7fdd24493c27bae848a554fd
khuzaimafilla <87953857+khuzaimafilla@users.noreply.github.com>
2023-10-24 20:43:04 +0700

    Update penambahan fitur pemilihan class kamar

--- bookingHotel.java
+++ bookingHotel.java
@@ -8,11 +8,13 @@ public class bookingHotel {
 
         // Deklarasi Variabel
         String nama, asal;
-        int usia, lama_menginap;
-        long tarif, total;
+        int usia, lama_menginap, jmlTamu;
+        long tarif1, tarif2, tarif3, total;
 
         // Isi Variabel
-        tarif = 250000;
+        tarif1 = 500000;
+        tarif2 = 1000000;
+        tarif3 = 1500000;
 
         // Syntax input
         System.out.println("Masukkan nama : ");
@@ -21,22 +23,47 @@ public class bookingHotel {
         usia = hotel.nextInt();
         System.out.println("Masukkan asal daerah/domisili anda : ");
         asal = hotel.next();
-        System.out.println("Masukkan waktu lama menginap anda : ");
+        System.out.println("Masukkan waktu lama menginap anda (Malam) : ");
         lama_menginap = hotel.nextInt();
+        System.out.println("Masukkan jumlah tamu dalam satu kamar : ");
+        jmlTamu = hotel.nextInt();
 
-        // Syntax perhitungan total biaya hotel
-        total = tarif * lama_menginap;
-
-        // Syntax menampilkan output
-        System.out.println("\n=== DETAIL PEMESANAN ===\n");
-        System.out.println("Nama : " + nama);
-        System.out.println("Usia : " + usia + " Tahun");
-        System.out.println("Asal Daerah : " + asal + "\n");
-        System.out.println("Pelanggan " + nama + " menginap selama : " + lama_menginap + " malam");
-        System.out.println("Biaya per malam : Rp " + tarif + ",00");
-        System.out.println(tarif + " x " + lama_menginap + " = " + total);
-        System.out.println("Total biaya pemesanan Hotel : Rp " + total + ",00\n");
-        System.out.println("=== TERIMAKASIH TELAH MENGINAP DI HOTEL KAMI !!! === \n *pastikan semua barang dan perlengkapan anda tidak tertinggal di kamar maupun area hotel \n *pihak hotel tidak akan bertanggungjawab atas segala kehilangan yang dialami pelanggan\n");
+        if (jmlTamu <= 4) {
+            System.out.println("1. Standard \nRincian Kamar : \nHarga : 500.000/nett \nFasilitas : AC, TV \n\n\n2. Superior \nRincian Kamar : \nHarga : 1.000.000/nett \nFasilitas : AC, TV, Kolam renang \n\n\n3. Delux \nRincian Kamr : \nHarga : 1.500.000/nett \nFasilitas : Ruang tamu, triple AC, TV, Pantry");
+            System.out.println("Pilih jenis kamar : ");
+            int menu;
+            menu = hotel.nextInt();
+            if (menu == 1){
+                total = lama_menginap * tarif1;
+                System.out.println("\n=== DETAIL PEMESANAN ===\n");
+                System.out.println("Nama : " + nama);
+                System.out.println("Usia : " + usia + " Tahun");
+                System.out.println("Asal Daerah : " + asal);
+                System.out.println("Total pembayaran selama " + lama_menginap + " malam : Rp" + total);
+                System.out.println("=== TERIMAKASIH TELAH MENGINAP DI HOTEL KAMI !!! === \n *pastikan semua barang dan perlengkapan anda tidak tertinggal di kamar maupun area hotel \n *pihak hotel tidak akan bertanggungjawab atas segala kehilangan yang dialami pelanggan\n");
+            } else if (menu == 2){
+                System.out.println("Harga kamar Superior per malam 1000000");
+                total = lama_menginap * tarif2;
+                System.out.println("\n=== DETAIL PEMESANAN ===\n");
+                System.out.println("Nama : " + nama);
+                System.out.println("Usia : " + usia + " Tahun");
+                System.out.println("Asal Daerah : " + asal);
+                System.out.println("Total pembayaran selama " + lama_menginap + " malam : Rp" + total);
+                System.out.println("=== TERIMAKASIH TELAH MENGINAP DI HOTEL KAMI !!! === \n *pastikan semua barang dan perlengkapan anda tidak tertinggal di kamar maupun area hotel \n *pihak hotel tidak akan bertanggungjawab atas segala kehilangan yang dialami pelanggan\n");
+            } else {
+                System.out.println("Harga kamar Delux per malam 1500000");
+                total = lama_menginap * tarif3;
+                System.out.println("\n=== DETAIL PEMESANAN ===\n");
+                System.out.println("Nama : " + nama);
+                System.out.println("Usia : " + usia + " Tahun");
+                System.out.println("Asal Daerah : " + asal);
+                System.out.println("Total pembayaran selama " + lama_menginap + " malam : Rp" + total);
+                System.out.println("=== TERIMAKASIH TELAH MENGINAP DI HOTEL KAMI !!! === \n *pastikan semua barang dan perlengkapan anda tidak tertinggal di kamar maupun area hotel \n *pihak hotel tidak akan bertanggungjawab atas segala kehilangan yang dialami pelanggan\n");
+            }
+            
+        } else {
+            System.out.println("Dilarang membawa tamu lebih dari 4");
+        }
     }
     
 }
